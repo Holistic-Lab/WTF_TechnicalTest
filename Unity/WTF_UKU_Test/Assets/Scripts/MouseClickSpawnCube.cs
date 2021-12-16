e64f7cd8c116a662114a84f6e30188007638c72d
Karim Guennoun <holisticlab.vr@gmail.com>
2021-12-16 15:51:21 +0100

    Clean and Add comment to MouseClickSpawnCube.cs

--- Unity/WTF_UKU_Test/Assets/Scripts/MouseClickSpawnCube.cs
+++ Unity/WTF_UKU_Test/Assets/Scripts/MouseClickSpawnCube.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90acf52c5352432695e4c228554cbef4b81db7fbd4dd824da3577239fe342750
-size 2590
+oid sha256:25efabae563b331804da2fa886da5854226d7890dd2c38da1377707444647824
+size 2688
